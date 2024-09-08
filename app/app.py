8a3d4b44c4b1defd8dd43e527a270568480c9ad6
kkm <kahmeng2001@gmail.com>
2024-09-09 00:21:29 +0800

    change abit

--- app/app.py
+++ app/app.py
@@ -2,23 +2,38 @@ import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output
 import psycopg2
+from flask import Flask, send_from_directory
+
+
+# Initialize Flask server
+server = Flask(__name__)
 
 # Initialize Dash app (Dash uses Flask under the hood)
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, server=server)
 
 # Define the layout of the app (this is the HTML structure)
 app.layout = html.Div(children=[
-    html.H1('Traffic Monitoring Dashboard'),
+    html.H1('Traffic Monitoring Dashboard App'),
     
     # A simple display for testing
     html.Div(id='db-status', children='PostgreSQL Connection: Unknown'),
-
+    
+    dcc.Link('Go to Main', href='/templates/main.html'),
     # A button to check the PostgreSQL connection
     html.Button('Check PostgreSQL Connection', id='check-connection-button'),
     
     # A paragraph that will update with the connection status
     html.Div(id='connection-result')
 ])
+
+# Root 
+@server.route('/')
+def serve_index():
+    return send_from_directory('templates', 'index.html')
+
+@server.route('/templates/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('templates', filename)
 
 # PostgreSQL connection function
 def connect_db():
@@ -50,4 +65,4 @@ def update_db_status(n_clicks):
 
 # Run the Dash server
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=5000)
+    server.run(debug=True, host='0.0.0.0', port=5000)
