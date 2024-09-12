dca3b83647af72eb79db81ae6862c02ef663cc21
kkm <kahmeng2001@gmail.com>
2024-09-12 17:42:51 +0800

    commnit

--- config/postgres_config.py
+++ config/postgres_config.py
@@ -1,8 +1,8 @@
 # Configuration for PostgreSQL connection
 POSTGRES_DB = {
     'dbname': 'traffic_db', # Default database
-    'user': 'admin',   # User from docker-compose
-    'password': 'admin',  # Password from docker-compose
+    'user': 'traffic_admin',   # User from docker-compose
+    'password': 'traffic_pass',  # Password from docker-compose
     'host': 'localhost',  # Host as configured in docker-compose
     'port': '5432'
 }
@@ -11,8 +11,8 @@ POSTGRES_DB = {
 SPARK_POSTGRES = {
     'url': 'jdbc:postgresql://localhost:5432/traffic_db',
     'properties': {
-        'user': 'admin',
-        'password': 'admin',
+        'user': 'traffic_admin',
+        'password': 'traffic_pass',
         'driver': 'org.postgresql.Driver'
     }
 }
