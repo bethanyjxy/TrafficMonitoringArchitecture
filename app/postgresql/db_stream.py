8cb3e111e524ef6bcf73d6bb737660ac2051ddc5
kkm <kahmeng2001@gmail.com>
2024-11-08 17:57:11 +0800

    fiux vms

--- app/postgresql/db_stream.py
+++ app/postgresql/db_stream.py
@@ -50,8 +50,8 @@ def fetch_stream_table(table_name):
     # Use sql.Identifier for safe table and column name injection
     query = sql.SQL("""
         SELECT * FROM {table}
-        WHERE ({timestamp_column}::timestamp AT TIME ZONE 'Asia/Singapore') >= %s
-        LIMIT 500;
+        WHERE (TO_TIMESTAMP({timestamp_column}, 'YYYY-MM-DD HH24:MI:SS') AT TIME ZONE 'Asia/Singapore') >= %s
+        LIMIT 500
     """).format(
         table=sql.Identifier(table_name),
         timestamp_column=sql.Identifier(timestamp_column)
