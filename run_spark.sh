3b5d9b1f0f6f28551e56813084b8d6d1ac0749bc
kkm <kahmeng2001@gmail.com>
2024-11-23 23:01:05 +0800

    delete not used files

--- run_spark.sh
+++
@@ -1,6 +0,0 @@
-echo "Starting Kafka to Spark Streaming..."
-
-spark-submit \
-  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1 \
-  --jars /opt/spark/jars/postgresql-42.2.18.jar \
-  app/spark/realtime/postgres_stream.py
